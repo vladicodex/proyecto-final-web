3973dd9b792d2be02413cc1e68a286a6c454d1c2
Vladimir <vladimiremiliojc@outlook.com>
2022-04-20 02:02:15 -0400

    This commits adds other pages

--- js/main.js
+++ js/main.js
@@ -1,22 +1,42 @@
-var abogadosNombre = [];
-var abogadosUsuario = [];
-var abogadosClave = [];
+function submitLogin() {
 
-function cargarAbogados() {
-    fetch('../db/abogados.json')
-        .then(respuesta => respuesta.json())
-        .then(abogados => {
-            abogados.forEach(abogado => {
-                var nombre = abogado.nombre;
-                var usuario = abogado.usuario;
-                var clave = abogado.clave;
+    var abogadosNombre = [];
+    var abogadosUsuario = [];
+    var abogadosClave = [];
 
-                abogadosNombre[abogado.id] = nombre;
-                abogadosUsuario[abogado.id] = usuario;
-                abogadosClave[abogado.id] = clave;
+    var username = document.getElementById('username-field').value;
+    var password = document.getElementById('password-field').value;
+
+    var accessDenied = true;
+
+    function cargarAbogados() {
+        fetch('../db/abogados.json')
+            .then(respuesta => respuesta.json())
+            .then(abogados => {
+                abogados.forEach(abogado => {
+                    var nombre = abogado.nombre;
+                    var usuario = abogado.usuario;
+                    var clave = abogado.clave;
+
+                    abogadosNombre[abogado.id] = nombre;
+                    abogadosUsuario[abogado.id] = usuario;
+                    abogadosClave[abogado.id] = clave;
+
+                    if((username == abogadosUsuario[abogado.id]) && (password == abogadosClave[abogado.id])){
+
+                        accessDenied = false;
+                        window.open("../oficinavirtual.html");
+            
+                    }
+                })
+
+                if(accessDenied == true && username != "" && password != ""){
+                    alert("❌ ACCESO DENEGADO ❌");
+                }
 
             })
-        })
-}
+    }
 
-cargarAbogados();
+    cargarAbogados();
+
+}
